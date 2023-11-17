6b6dd92a5a81e31703542396ec83a527aef56d90
seredomi <serenomdominguez@gmail.com>
2023-11-17 22:18:14 +0900

    base code for Personnel Page

---
+++ BarracksScanner/PersonnelPage.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "PersonnelPage.h"
+#if __has_include("PersonnelPage.g.cpp")
+#include "PersonnelPage.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+namespace winrt::BarracksScanner::implementation
+{
+    int32_t PersonnelPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void PersonnelPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void PersonnelPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        Button().Content(box_value(L"Personnel Clicked"));
+    }
+}
---
+++ BarracksScanner/PersonnelPage.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "PersonnelPage.g.h"
+
+namespace winrt::BarracksScanner::implementation
+{
+    struct PersonnelPage : PersonnelPageT<PersonnelPage>
+    {
+        PersonnelPage() 
+        {
+            // Xaml objects should not call InitializeComponent during construction.
+            // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+        }
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::BarracksScanner::factory_implementation
+{
+    struct PersonnelPage : PersonnelPageT<PersonnelPage, implementation::PersonnelPage>
+    {
+    };
+}
