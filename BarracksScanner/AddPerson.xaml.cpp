8487b08f670e800d90c6b500a62e19b464575e89
seredomi <serenomdominguez@gmail.com>
2023-12-18 16:01:45 -0800

    base code for add person content dialogue

---
+++ BarracksScanner/AddPerson.xaml.cpp
@@ -0,0 +1,35 @@
+﻿//
+// AddPerson.xaml.cpp
+// Implementation of the AddPerson class
+//
+
+#include "pch.h"
+#include "AddPerson.xaml.h"
+
+using namespace BarracksScanner;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+BarracksScanner::AddPerson::AddPerson()
+{
+	InitializeComponent();
+}
+
+void BarracksScanner::AddPerson::ContentDialog_PrimaryButtonClick(Windows::UI::Xaml::Controls::ContentDialog^ sender, Windows::UI::Xaml::Controls::ContentDialogButtonClickEventArgs^ args)
+{
+}
+
+void BarracksScanner::AddPerson::ContentDialog_SecondaryButtonClick(Windows::UI::Xaml::Controls::ContentDialog^ sender, Windows::UI::Xaml::Controls::ContentDialogButtonClickEventArgs^ args)
+{
+}
---
+++ BarracksScanner/AddPerson.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// AddPerson.xaml.h
+// Declaration of the AddPerson class
+//
+
+#pragma once
+
+#include "AddPerson.g.h"
+
+namespace BarracksScanner
+{
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class AddPerson sealed
+	{
+	public:
+		AddPerson();
+	private:
+		void ContentDialog_PrimaryButtonClick(Windows::UI::Xaml::Controls::ContentDialog^ sender, Windows::UI::Xaml::Controls::ContentDialogButtonClickEventArgs^ args);
+		void ContentDialog_SecondaryButtonClick(Windows::UI::Xaml::Controls::ContentDialog^ sender, Windows::UI::Xaml::Controls::ContentDialogButtonClickEventArgs^ args);
+	};
+}
